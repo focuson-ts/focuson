d711a93c4160daa17b27934d4b6ea4eb97e4c2d8
phil <phil.rice@validoc.org>
2022-02-21 10:17:47 +0100

    updateing version number

--- modules/formJava/java/ExampleApp/src/main/java/focuson/data/Wiring.java
+++ modules/formJava/java/ExampleApp/src/main/java/focuson/data/Wiring.java
@@ -45,7 +45,7 @@ public class Wiring {
           .type(newTypeWiring("Query").dataFetcher("getEAccountsSummaryDD", fetchers.getEAccountsSummaryDD()))
           .type(newTypeWiring("Mutation").dataFetcher("createETransferDataD", fetchers.createETransferDataD()))
           .type(newTypeWiring("Mutation").dataFetcher("createCreateEAccountDataDD", fetchers.createCreateEAccountDataDD()))
-          .type(newTypeWiring("OneLineStringDD").dataFetcher("getAccountSummaryDescription", fetchers.getAccountSummaryDescription()))
+          .type(newTypeWiring("EACcounta").dataFetcher("getAccountSummaryDescription", fetchers.getAccountSummaryDescription()))
           .type(newTypeWiring("IntegerDD").dataFetcher("getTotalMonthlyCost", fetchers.getTotalMonthlyCost()))
           .type(newTypeWiring("IntegerDD").dataFetcher("getOneAccountBalance", fetchers.getOneAccountBalance()))
           .type(newTypeWiring("IntegerDD").dataFetcher("getCurrentAccountBalance", fetchers.getCurrentAccountBalance()))
