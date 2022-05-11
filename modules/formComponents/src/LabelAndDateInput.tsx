97a032a9bc2c3cd31e9ef59ee17d035daba73ef6
latika2016 <latika.mailbox@gmail.com>
2022-05-11 11:44:49 +0200

    Added date interval to datepicker

--- modules/formComponents/src/LabelAndDateInput.tsx
+++ modules/formComponents/src/LabelAndDateInput.tsx
@@ -4,7 +4,7 @@ import { FocusOnContext } from "@focuson/focuson";
 import { CommonStateProps } from "./common";
 import { Label } from "./label";
 import { makeButtons } from "./labelAndInput";
-import { cleanInputProps } from "./input";
+// import { cleanInputProps } from "./input";
 import DatePicker from "react-datepicker";
 
 
@@ -13,19 +13,17 @@ export interface LabelAndDateProps<S, Context> extends CommonStateProps<S, strin
   readonly?: boolean;
   allButtons: NameAnd<JSX.Element>;
   buttons?: string[]
-  datesExcluded?: LensState<S,any[], Context>
+  datesExcluded?: LensState<S,any[], Context>,
+  fieldNameInHolidays?: string,
+  workingDaysInFuture?: number,
 }
 
 export function LabelAndDateInput<S, T, Context extends FocusOnContext<S>> ( props: LabelAndDateProps<S, Context> ) {
-  const { state, ariaLabel, id, mode, label, name, buttons, readonly, datesExcluded } = props
+  const { state, ariaLabel, id, mode, label, name, buttons, readonly, datesExcluded, fieldNameInHolidays, workingDaysInFuture } = props
 
-  const datesToExclude = datesExcluded?.optJsonOr([]).map(d => d['holiday'])
-  
-  console.log('props.datesExcluded', props.datesExcluded?.optJsonOr([]), datesToExclude)
-
-  // const datesExcluded = ['2022-05-30','2022-05-04','2022-05-17','2022-05-20','2022-05-26','2022-05-27']
-  // const datesExcluded = [new Date('2022-05-09'), new Date('2022-05-04'), new Date('2022-05-17'), new Date('2022-05-20'), new Date('2022-05-26'), new Date('2022-05-27')]  
-  // const datesExcluded = ['30-05-2022','04-05-2022','17-05-2022','20-05-2022','26-05-2022','27-05-2022']
+  const datesToExclude = datesExcluded?.optJsonOr([]).map(d => d[fieldNameInHolidays? fieldNameInHolidays : 'holiday'])  
+  // const datesToExcludeAsDate = safeArray(datesToExclude).map(d => new Date(d)) 
+  // const highlightDatesToExclude = [{"react-datepicker__day--highlighted-custom-2": datesToExcludeAsDate}];
 
   const isHoliday = (date: any, dateList: any) => {
     return dateList.find((d:any) => new Date(d).toDateString() === date.toDateString());
@@ -36,21 +34,40 @@ export function LabelAndDateInput<S, T, Context extends FocusOnContext<S>> ( pro
   }
 
   const onChange = ( date: any ) => {
-    console.log('onchange', date)
     if(date) state.setJson ( date.toDateString(), reasonFor ( 'LabelAndDate', 'onChange', id ) )
   };
 
   const isWeekday = (date: any) => {
-    const day = date.getDay();
-    return day !== 0 && day !== 6;
+    const day = date.getDay()
+    return day !== 0 && day !== 6
   };
 
+  const addDays = (date: Date, n: number) => {
+    let count: number = 0
+    let endDate: Date = null
+    while(count < n){
+      endDate = new Date(date.setDate(date.getDate() + 1))
+      if(isWeekday(endDate)) count++
+    }
+    return endDate
+  }
+  const subDays = (date: Date, n: number) => {
+    let count: number = 0
+    let endDate: Date = null
+    while(count < n){
+      endDate = new Date(date.setDate(date.getDate() - 1))
+      if(isWeekday(endDate)) count++
+    }
+    return endDate
+  }
+  
   const selectedDate = new Date(state.optJsonOr(new Date().toDateString())) // TODO: Turn default date into arg
   return (<div className='labelAndDate'>
       <Label state={state} htmlFor={name} label={label}/>
       {/* <input {...cleanInputProps ( props )} type='date' readOnly={mode === 'view' || readonly} onChange={onChange} value={state.optJsonOr ( '' )}/> */}
-      <DatePicker selected={selectedDate} onChange={(date) => onChange(date)} filterDate={(date) => isExcluded(date, safeArray(datesToExclude))}
-      // ?.optJson()))} 
+      <DatePicker selected={selectedDate} onChange={(date) => onChange(date)} filterDate={(date) => isExcluded(date, safeArray(datesToExclude))} 
+      includeDateIntervals={[{ start: subDays(new Date(), 5), end: addDays(new Date(), 5) },]}
+      // highlightDates={datesToExcludeAsDate}
       placeholderText="Select a weekday"/>
       {makeButtons ( props.allButtons, props.buttons )}
     </div>)
