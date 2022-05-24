818015b034fe451f4c58117ab864f530231af207
phil <phil.rice@validoc.org>
2022-05-24 10:14:03 +0200

    making it so that examples and tests are not published in @focuson/forms

--- modules/forms/src/appConfig.ts
+++ modules/forms/src/appConfig.ts
@@ -8,7 +8,8 @@ export interface AppConfig {
   debug: any;
   versionNumber: string;
   tsPort: number;
-  javaPort: number;}
+  javaPort: number;
+}
 
 
 export const javaOutputRoot = '../formJava'
@@ -16,14 +17,14 @@ export const tsRoot = "../formTs"
 export const focusOnVersion: string = JSON.parse ( loadFile ( 'package.json' ) ).version
 
 const MyCombineCD: SimpleDisplayComp = { import: "@focuson/form_components", name: "MyCombined" }
-
-const details = JSON.parse ( loadFile ( 'project.details.json' ) ).details;
-export const javaPort = details.javaPort
-export const tsPort: number = details.tsPort
-if ( !javaPort || !tsPort ) {
-  console.log ( "exiting. please define javaPort and tsPort in the project.details file, in the details section" );
-  process.exit ( 2 )
+let details: any = undefined;
+try {
+  details = JSON.parse ( loadFile ( 'project.details.json' ) ).details;
+} catch ( e: any ) {
 }
+export const javaPort = details?.javaPort ? details.javaPort : 8080
+export const tsPort: number = details?.tsPort ? details.tsPort : 3000
+
 console.log ( "JavaPort is", javaPort, "tsPort is", tsPort )
 
 export const devAppConfig: AppConfig = {
@@ -32,8 +33,10 @@ export const devAppConfig: AppConfig = {
   versionNumber: '0.0.1',
   fetch: `fetchWithDelay ( 1, fetchWithPrefix ( 'http://localhost:${javaPort}', loggingFetchFn ) )`,
   combine: MyCombineCD,
-  debug: { fetcherDebug: false, guardDebug: false, restDebug: false, selectedPageDebug: false,
+  debug: {
+    fetcherDebug: false, guardDebug: false, restDebug: false, selectedPageDebug: false,
     loadTreeDebug: false, showTracing: false,
-    recordTrace: true, tagFetcherDebug: false, accordions: [] }
+    recordTrace: true, tagFetcherDebug: false, accordions: []
+  }
 }
 
