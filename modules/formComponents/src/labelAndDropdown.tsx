8b2e1a240d98131920a627cb7ed7ece9b0fb9183
latika2016 <latika.mailbox@gmail.com>
2022-06-22 21:54:48 +0200

    Css fix in LabelAndDropwdown

--- modules/formComponents/src/labelAndDropdown.tsx
+++ modules/formComponents/src/labelAndDropdown.tsx
@@ -20,28 +20,24 @@ export interface LabelAndDropdownProps<S, T, Context> extends DropdownProps<S, T
 }
 
 export function LabelAndDropdown<S, T, Context extends FocusOnContext<S>> ( props: LabelAndDropdownProps<S, string, Context> ) {
-  const { enums, state, ariaLabel, id, mode, label, name, buttons, readonly, pleaseSelect, size, required } = props
-  let selected = state.optJson ();
-  const hasValid = selected && Object.keys ( enums ).includes ( selected )
-  const value = hasValid ? selected : undefined
-  const pleaseSelectClass = hasValid ? '' : ' pleaseSelect'
+  const { state, label, name, buttons, allButtons } = props
   return (<div className={`dropdown-container ${props.labelPosition == 'Horizontal'? 'd-flex-inline' : ''}`}>
       <Label state={state} htmlFor={name} label={label}/>
-      <div className={`${props.buttons && props.buttons.length > 0 ? 'dropdownAndButtons' : ''}`}>
-        <Dropdown{...props} />{makeButtons ( props.allButtons, props.buttons )}
+      <div className={`${buttons && buttons.length > 0 ? 'dropdownAndButtons' : ''}`}>
+        <Dropdown{...props} />{makeButtons ( allButtons, buttons )}
       </div>
     </div>
   )
 }
 
 export function Dropdown<S, T, Context extends FocusOnContext<S>> ( props: DropdownProps<S, string, Context> ) {
-  const { enums, state, ariaLabel, id, mode, name, buttons, readonly, pleaseSelect, size, required } = props
+  const { enums, state, ariaLabel, id, mode, readonly, pleaseSelect, size, required } = props
   let selected = state.optJson ();
   const hasValid = selected && Object.keys ( enums ).includes ( selected )
   const value = hasValid ? selected : undefined
-  const pleaseSelectClass = hasValid ? '' : ' invalid'
+  const cssValidInput = hasValid || !required ? '' : ' invalid'
   return (
-    <select className={`select ${pleaseSelectClass}`} value={value} disabled={mode === 'view' || readonly} id={id} required={required} size={size} aria-label={ariaLabel} onChange={( e ) =>
+    <select className={`select ${cssValidInput}`} value={value} disabled={mode === 'view' || readonly} id={id} required={required} size={size} aria-label={ariaLabel} onChange={( e ) =>
       state.setJson ( e.target.value, reasonFor ( 'LabelAndDropdown', 'onChange', id ) )}>
       {pleaseSelect && !hasValid && <option>{pleaseSelect}</option>}
       {
