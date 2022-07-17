eb76ba197ce30ec8e7acff4bd85f57d926f801c8
phil <phil.rice@validoc.org>
2022-07-17 14:44:24 +0200

    wip: datepicker

--- modules/lenstest/src/labelAndDateInput.spec.tsx
+++ modules/lenstest/src/labelAndDateInput.spec.tsx
@@ -5,23 +5,23 @@ const firstMay2020 = new Date ( '2020/05/01' )
 
 describe ( "parseDate", () => {
   it ( "should parse yyyy/MM/dd", () => {
-    expect ( parseDate ( 'yyyy/MM/dd' ) ( '2020/05/01' ) ).toEqual ( firstMay2020 )
-    expect ( parseDate ( 'yyyy/MM/dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ('prefix', 'yyyy/MM/dd' ) ( '2020/05/01' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','yyyy/MM/dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
   } )
   it ( "should parse yyyy-MM-dd", () => {
-    expect ( parseDate ( 'yyyy-MM-dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
-    expect ( parseDate ( 'yyyy-MM-dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','yyyy-MM-dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','yyyy-MM-dd' ) ( '2020-05-01' ) ).toEqual ( firstMay2020 )
   } )
   it ( "should parse dd/yyyy/MM", () => {
-    expect ( parseDate ( 'dd/yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
-    expect ( parseDate ( 'dd/yyyy-MM' ) ( '01/2020/05' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd/yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd/yyyy-MM' ) ( '01/2020/05' ) ).toEqual ( firstMay2020 )
   } )
   it ( "should parse dd-yyyy-MM", () => {
-    expect ( parseDate ( 'dd-yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
-    expect ( parseDate ( 'dd-yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd-yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd-yyyy-MM' ) ( '01-2020-05' ) ).toEqual ( firstMay2020 )
   } )
   it ( "should parse dd/MM/yyyy", () => {
-    expect ( parseDate ( 'dd-MM-yyyy' ) ( '01-05-2020' ) ).toEqual ( firstMay2020 )
-    expect ( parseDate ( 'dd-MM-yyyy' ) ( '01/05/2020' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd-MM-yyyy' ) ( '01-05-2020' ) ).toEqual ( firstMay2020 )
+    expect ( parseDate ( 'prefix','dd-MM-yyyy' ) ( '01/05/2020' ) ).toEqual ( firstMay2020 )
   } )
 } )
