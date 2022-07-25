86e8fa4bc5976bbd4625897e2811f43e3fefa190
phil <phil.rice@validoc.org>
2022-07-25 19:10:11 +0100

    wip: moving replace code around

--- modules/lens/src/path.ts
+++ modules/lens/src/path.ts
@@ -147,3 +147,19 @@ export function processPath<Build> ( s: ParseState<Build>, p: PathBuilder<Build>
   }
 }
 
+export const replaceTextFn = <S> ( errorPrefix: string, s: S, from: ( path: string ) => Optional<S, any> ) => ( f: string ) => {
+  const parts = f.slice ( 1, -1 ).split ( "|" )
+  const value = from ( parts[ 0 ] ).getOption ( s )
+  if ( parts.length == 1 ) return `${value}`
+  if ( typeof value == 'boolean' ) {
+    if ( parts.length == 3 ) return value ? parts[ 2 ] : parts[ 1 ]
+    throw new Error ( `${errorPrefix} Replacing string ${f} and it's a boolean [${value}], but there are ${parts.length - 1} options, not 2` )
+  }
+  if ( typeof value == 'number' ) {
+    if ( value >= 0 && value <= parts.length ) return parts[ value + 1 ]
+    throw new Error ( `${errorPrefix} Replacing string ${f} and it's a string [${value}], but there are ${parts.length - 1} options, and this value is out of range` )
+  }
+};
+export function replaceTextUsingPathForS<S> ( errorPrefix: string, s: S, fromPath: ( path: string ) => Optional<S, any>, string: string ): string {
+  return string.replace ( /{[^}]*}/g, replaceTextFn ( errorPrefix, s, fromPath ) );
+}
--- modules/pages/src/replace.ts
+++ modules/pages/src/replace.ts
@@ -1,22 +1,8 @@
 import { LensState } from "@focuson/state";
 import { fromPathGivenState, PageSelectionContext } from "./pageSelection";
+import { replaceTextFn } from "@focuson/lens";
 
 
-export function replaceTextUsingPath<S, Context extends PageSelectionContext<S>> ( state: LensState<S, any, Context>, label: string ) {
-  const from = fromPathGivenState ( state )
-  function replaceIt ( f: string ) {
-    const parts = f.slice ( 1, -1 ).split ( "|" )
-    const value = from ( parts[ 0 ] ).getOption ( state.main )
-    if ( parts.length == 1 ) return `${value}`
-    if ( typeof value == 'boolean' ) {
-      if ( parts.length == 3 ) return value ? parts[ 2 ] : parts[ 1 ]
-      throw new Error ( `Label is ${label} Replacing string ${f} and it's a boolean [${value}], but there are ${parts.length - 1} options, not 2` )
-    }
-    if ( typeof value == 'number' ) {
-      if ( value >= 0 && value <= parts.length ) return parts[ value + 1 ]
-      throw new Error ( `Label is ${label} Replacing string ${f} and it's a string [${value}], but there are ${parts.length - 1} options, and this value is out of range` )
-    }
-  }
-  const text = label.replace ( /{[^}]*}/g, replaceIt )
-  return text;
-}
+export const replaceTextUsingPath = <S, Context extends PageSelectionContext<S>> ( state: LensState<S, any, Context>, label: string ) =>
+  label.replace ( /{[^}]*}/g, replaceTextFn ( `Label is ${label}`, state.main, fromPathGivenState ( state ) ) );
+
