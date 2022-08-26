af5d4ababc09266d964c22378e42bf84ac944a4f
phil <phil.rice@validoc.org>
2022-08-26 08:32:13 +0100

    wip: more decoupling of pages

--- modules/forms/src/example/authoriseCharges/authoriseCharges.customise.ts
+++ modules/forms/src/example/authoriseCharges/authoriseCharges.customise.ts
@@ -5,16 +5,19 @@ export interface AuthoriseCustomisation{
   pageName : string
   authoriseTable: DBTable
   urlPrefix: string
+  namePrefix: string
 }
 
 export const DirectDebitAC: AuthoriseCustomisation ={
   pageName: 'AuthoriseCharges',
   authoriseTable: authorisedChargesTableDD,
+  namePrefix: 'ac',
   urlPrefix: '/api/dd'
 }
 export const CreditAC: AuthoriseCustomisation ={
   pageName: 'Credit',
   authoriseTable:authorisedChargesTableDD,
+  namePrefix: 'cc',
   urlPrefix: '/api/cc'
 
 }
--- modules/forms/src/example/authoriseCharges/authoriseCharges.restD.ts
+++ modules/forms/src/example/authoriseCharges/authoriseCharges.restD.ts
@@ -2,28 +2,30 @@ import { ExampleRestD } from "../common";
 import { ListOfChargesDD, SelectOneBrandDD, summaryOfChargesDateTableDD } from "./authoriseCharges.dataD";
 import { fromCommonIds } from "../commonIds";
 import { StringParam } from "../../common/restD";
-import { authorisedChargesTableDD, onlySchema } from "../database/tableNames";
+import { onlySchema } from "../database/tableNames";
 import { AuthoriseCustomisation } from "./authoriseCharges.customise";
 
 
-export function SelectOneBrandPageRD (c: AuthoriseCustomisation): ExampleRestD {
+export function SelectOneBrandPageRD ( c: AuthoriseCustomisation ): ExampleRestD {
   return {
-    dataDD: SelectOneBrandDD(c),
+    dataDD: SelectOneBrandDD ( c ),
     params: {},
+    namePrefix: c.namePrefix,
     url: `${c.urlPrefix}/brand/list`,
     actions: [ 'get' ],
   }
 }
 
 
-export function AuthorisedChargesRD (c: AuthoriseCustomisation): ExampleRestD {
+export function AuthorisedChargesRD ( c: AuthoriseCustomisation ): ExampleRestD {
   return {
-    dataDD: ListOfChargesDD(c),
+    dataDD: ListOfChargesDD ( c ),
     params: {
       ...fromCommonIds ( 'brandRef', 'clientRef' ),
       date: { ...StringParam, lens: '~/authorisedCharges/date', testValue: '2022-12-1' }
     },
     url: `${c.urlPrefix}/charges/summary?{query}`,
+    namePrefix: c.namePrefix,
     actions: [ 'get', 'update' ],
     tables: {
       noDataIs404: true,
@@ -33,73 +35,78 @@ export function AuthorisedChargesRD (c: AuthoriseCustomisation): ExampleRestD {
   }
 }
 
-export function SummaryOfChargeDatesRD (c: AuthoriseCustomisation): ExampleRestD {
-  return {
-    dataDD: summaryOfChargesDateTableDD(c),
+export function SummaryOfChargeDatesRD ( c: AuthoriseCustomisation ): ExampleRestD {
+  const result: ExampleRestD = {
+    dataDD: summaryOfChargesDateTableDD ( c ),
+    namePrefix: c.namePrefix,
     params: {
       ...fromCommonIds ( 'brandRef', 'clientRef' ),
       date: { ...StringParam, lens: '~/summaryOfChargesDates/date', testValue: '2022-12-1' }
     },
     url: `${c.urlPrefix}l/charges/dates?{query}`,
     actions: [ 'get' ],
-    resolvers: {
-      getSummaryOfChargesDate: {
-        type: 'case',
-        name: 'getTheStuff',
-        params: [ 'brandRef' ],
-        list: true,
-        select: [
+    resolvers: {}
+    // getSummaryOfChargesDate:
+  }
+  const name = `get${c.namePrefix}SummaryOfChargesDate`
+  result.resolvers[ name ] = {
+    type: 'case',
+    name: 'getTheStuff',
+    params: [ 'brandRef' ],
+    list: true,
+    select: [
+      {
+        guard: [], type: 'multiple', mutations: [
           {
-            guard: [], type: 'multiple', mutations: [
-              {
-                type: 'sql', schema: onlySchema, sql: 'select something from somewhere', params: [
-                  { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
-                ]
-              },
-              {
-                type: 'sql', schema: onlySchema, sql: 'somesql', params: [ 'brandRef', 'something',
-                  { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
-                ], list: true
-              },
-              { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+            type: 'sql', schema: onlySchema, sql: 'select something from somewhere', params: [
+              { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
+            ]
           },
           {
-            guard: [], type: 'sql', schema: onlySchema, sql: 'someOtherSql', params: [ 'brandRef',
-              { type: 'output', javaType: 'String', name: 'bill', rsName: 'bil' },
+            type: 'sql', schema: onlySchema, sql: 'somesql', params: [ 'brandRef', 'something',
               { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
             ], list: true
           },
+          { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+      },
+      {
+        guard: [], type: 'sql', schema: onlySchema, sql: 'someOtherSql', params: [ 'brandRef',
+          { type: 'output', javaType: 'String', name: 'bill', rsName: 'bil' },
+          { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
+        ], list: true
+      },
+      {
+        guard: [], type: 'multiple', mutations: [
           {
-            guard: [], type: 'multiple', mutations: [
-              {
-                type: 'sql', schema: onlySchema, sql: 'select something from somewhere', params: [
-                  { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
-                ]
-              },
-              {
-                type: 'sql', schema: onlySchema, sql: 'somesql', params: [ 'brandRef', 'something',
-                  { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
-                ], list: true
-              },
-              { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+            type: 'sql', schema: onlySchema, sql: 'select something from somewhere', params: [
+              { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
+            ]
           },
           {
-            guard: [], type: 'multiple', mutations: [
-              {
-                type: 'sql', schema: onlySchema, sql: 'select something from somewhere', noDataIs404: true, params: [
-                  { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
-                ]
-              },
-              {
-                type: 'sql', schema: onlySchema, sql: 'somesql', noDataIs404: true, params: [ 'brandRef', 'something',
-                  { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
-                ], list: true
-              },
-              { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+            type: 'sql', schema: onlySchema, sql: 'somesql', params: [ 'brandRef', 'something',
+              { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
+            ], list: true
           },
+          { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+      },
+      {
+        guard: [], type: 'multiple', mutations: [
+          {
+            type: 'sql', schema: onlySchema, sql: 'select something from somewhere', noDataIs404: true, params: [
+              { type: 'output', name: 'something', rsName: 'something', javaType: 'String' }
+            ]
+          },
+          {
+            type: 'sql', schema: onlySchema, sql: 'somesql', noDataIs404: true, params: [ 'brandRef', 'something',
+              { type: 'output', javaType: 'String', name: 'bob', rsName: 'bob' }
+            ], list: true
+          },
+          { type: 'sql', schema: onlySchema, sql: 'insert something in audit file', params: 'brandRef' } ]
+      },
 
-        ]
-      }
-    }
+    ]
   }
+  return result
 }
+
+
