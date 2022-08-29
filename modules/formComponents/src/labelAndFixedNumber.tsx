b17ede9c3dfffa021935f76c08ec60d733ffdd7b
phil <phil.rice@validoc.org>
2022-08-29 10:44:15 +0200

    LabelAndFixedNumber has an optional className

--- modules/formComponents/src/labelAndFixedNumber.tsx
+++ modules/formComponents/src/labelAndFixedNumber.tsx
@@ -2,7 +2,9 @@ export interface LabelAndFixedNumberProps {
   id: string
   label: string;
   number: string;
+  className?: string
 }
-export function LabelAndFixedNumber ( { id, label, number }: LabelAndFixedNumberProps ) {
-  return <div className='labelValueButton'><label className='input-label' htmlFor={id}>{label}</label><input id={id} className='input' readOnly type='number' value={number}/></div>
+export function LabelAndFixedNumber ( { id, label, number, className }: LabelAndFixedNumberProps ) {
+  const clazz = className ? className : 'input-label'
+  return <div className='labelValueButton'><label className={clazz} htmlFor={id}>{label}</label><input id={id} className='input' readOnly type='number' value={number}/></div>
 }
