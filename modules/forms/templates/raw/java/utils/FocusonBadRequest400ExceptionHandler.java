7a79c4f1989cd69de703fc48d9dc1ecfec307423
Lewis <lewisthornton@btinternet.com>
2023-01-18 13:43:50 +0000

    Added sping endpoint ordering to both focusonExceptionHandlers

--- modules/forms/templates/raw/java/utils/FocusonBadRequest400ExceptionHandler.java
+++ modules/forms/templates/raw/java/utils/FocusonBadRequest400ExceptionHandler.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
+
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class FocusonBadRequest400ExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(FocusonBadRequest400Exception.class)
     public ResponseEntity<Object> handleExceptions(FocusonBadRequest400Exception exception, WebRequest webRequest) {
--- modules/forms/templates/raw/java/utils/FocusonNotFound404ExceptionHandler.java
+++ modules/forms/templates/raw/java/utils/FocusonNotFound404ExceptionHandler.java
@@ -6,7 +6,11 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.Ordered;
+
 @ControllerAdvice
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class FocusonNotFound404ExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(FocusonNotFound404Exception.class)
     public ResponseEntity<Object> handleExceptions(FocusonNotFound404Exception exception, WebRequest webRequest) {
