e0d27528663f19a5386bd8b5b115e4391cddef8b
phil <phil.rice@validoc.org>
2023-10-24 10:32:00 +0200

    Starting to move to testinglibrary: deleting enzyme

--- modules/nav/src/Nav.spec.tsx
+++ modules/nav/src/Nav.spec.tsx
@@ -1,40 +1,46 @@
-//Copyright (c)2020-2022 Philip Rice. <br />Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the Software), to dealin the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  <br />The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED AS
+//Copyright (c)2020-2023 Philip Rice. <br />Permission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the Software), to dealin the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:  <br />The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED AS
 import React from 'react';
-import {Nav} from "./Nav";
-import {enzymeSetup} from './enzymeAdapterSetup';
-import {shallow} from "enzyme";
 
-enzymeSetup()
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Nav } from "./Nav";
 
-const flushPromises = () => new Promise(setImmediate);
-let jsonFiles = ['jsonFile1', 'jsonFile2'];
-
-function fetch(url: string) {
-    expect(url).toEqual(jsonFiles[0]);
-    return Promise.resolve('someResult')
-}
 describe("Nav", () => {
+    const mockJsonFiles = ['jsonFile1', 'jsonFile2'];
+    const mockDataResponse = 'someResult';
 
-    it('renders the files assed to it', () => {
-        const nav = shallow(<Nav jsonFiles={jsonFiles} setData={jest.fn()} fetch={fetch}/>)
-        expect(nav.find('.navitem').map(n => n.text())).toEqual(jsonFiles)
-        expect(nav.find('.navitem').map(n => n.getElement().type)).toEqual(['li', 'li'])
-        expect(nav.find('.navitem').map(n => n.getElement().key)).toEqual(jsonFiles)
+    // Mock fetch function to return our mock data
+    const mockFetch = jest.fn((url) => Promise.resolve(mockDataResponse));
+
+    beforeEach(() => {
+        // Clear all instances and calls to the mock function before each test
+        mockFetch.mockClear();
     });
 
-    it('on click should call setJson with the url', async () => {
-        let setData = jest.fn()
-        const nav = shallow(<Nav jsonFiles={jsonFiles} fetch={fetch} setData={setData}/>)
-        let item0 = nav.findWhere(n => n.key() === 'jsonFile1');
-        expect(item0.text()).toEqual('jsonFile1')
-        item0.simulate('click')
+    it('renders the files passed to it', () => {
+        const { getAllByRole } = render(<Nav jsonFiles={mockJsonFiles} setData={jest.fn()} fetch={mockFetch} />);
+        const navItems = getAllByRole('listitem');
 
-        await flushPromises();
-        nav.update();
+        // Check the number of rendered list items
+        expect(navItems).toHaveLength(mockJsonFiles.length);
 
-        expect(setData.mock.calls.length).toBe(1);
-        expect(setData.mock.calls[0][0]).toBe('someResult');
-        expect(item0.getElement().type).toEqual('li')
+        // Check text content
+        navItems.forEach((item, index) => {
+            expect(item).toHaveTextContent(mockJsonFiles[index]);
+        });
     });
 
-})
+    it('calls setData with fetched result on click', async () => {
+        const mockSetData = jest.fn();
+        const { getByText } = render(<Nav jsonFiles={mockJsonFiles} fetch={mockFetch} setData={mockSetData} />);
+
+        const itemToClick = getByText(mockJsonFiles[0]);
+        fireEvent.click(itemToClick);
+
+        // Check if fetch was called with the correct URL
+        expect(mockFetch).toHaveBeenCalledWith(mockJsonFiles[0]);
+
+        // The fetch should resolve and call setData
+        expect(mockSetData).toHaveBeenCalledWith(mockDataResponse);
+    });
+});
